a2c2a64607a53d38c8508e416f72cd1071e50c7a
refatull <refatull@buffalo.edu>
2023-05-29 13:03:33 -0400

    Initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="content">
+        <h1>Welcome to Cat Dating!</h1>
+        <p>Find your purr-fect match.</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
